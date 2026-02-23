55010b4ae8ca7042f941e35f8d85f64b4fcf1230
Carlos Felipe Santos <ccfelipe08@gmail.com>
2026-02-23 10:31:03 -0300

    .

--- src/components/LojaCard.js
+++ src/components/LojaCard.js
@@ -12,3 +12,17 @@ export default function LojaCard({loja}) {
         </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        flexDirection: 'row',   
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 16,
+        elevation: 3, //sombra para Android
+        shadowColor: '#000', //sombra para iOS
+        shadowOffset: { width: 0, height: 2 }, //sombra para iOS
+        shadowOpacity: 0.1, //sombra para iOS
+        shadowRadius: 4, //sombra para iOS
+    }
